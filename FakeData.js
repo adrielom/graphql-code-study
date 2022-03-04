6027f0b49ed257eb7c062bbc0f30aecba65b7251
Adriel Oliveira <adriel.oliveira@dellead.com>
2022-03-04 08:48:55 -0300

    initial commit

---
+++ FakeData.js
@@ -0,0 +1,37 @@
+module.exports =  UserList = [
+    {
+        id: 1,
+        name: 'Adriel',
+        username: 'adrielom',
+        age: 28,
+        nationality: 'Brazilian'
+    },
+    {
+        id: 2,
+        name: 'Johnny Depp',
+        username: 'jacksparrow',
+        age: 58,
+        nationality: 'Us'
+    },
+    {
+        id: 3,
+        name: 'keira knightley',
+        username: 'prideandprejudicegirl',
+        age: 36,
+        nationality: 'Us'
+    },
+    {
+        id: 4,
+        name: 'arnold schwarzenegger',
+        username: 'illbeback',
+        age: 74,
+        nationality: 'Austria'
+    },
+    {
+        id: 5,
+        name: 'Jackie Chan',
+        username: 'badday',
+        age: 67,
+        nationality: 'China'
+    },
+]
